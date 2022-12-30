18f9f5417e43737b928753a8558b8f73d44f8e81
Akshat Giri <akshatgiri@gmail.com>
2022-12-29 19:18:06 -0800

    Drink up 🍷🍸🍹🥃🍾

---
+++ README.md
@@ -0,0 +1,26 @@
+# Nodjs Script to get you hammered at the Fins.wtf Last Call
+
+You're a baller. You're a stud. You're on a mission to get wasted. You want all the 15 drinks from the bar but waiting in lines is for losers. You're gonna get your own private table and be the first to get a drinks.
+
+Here's how to use this script
+
+Clone the project
+
+```
+git clone https://github.com/AkshatGiri/fins.wtf-shark-bot.git
+```
+
+Create a .env file in the folder and plug in the values
+
+```
+INFURA_API_KEY=
+WALLET_PRIVATE_KEY=
+```
+
+Run the script
+
+```
+npm start
+```
+
+Lay back in the comfortable leather seats of your private table and let the hot javascript waitress bring you your drinks.
---
+++ private_table.js
@@ -0,0 +1,95 @@
+require('dotenv').config()
+const fs = require('fs')
+const { BigNumber, providers, Wallet, Contract, utils } = require('ethers')
+
+const { INFURA_API_KEY, WALLET_PRIVATE_KEY } = process.env
+
+const CONTRACT = {
+  abi: JSON.parse(fs.readFileSync('abi.json', 'utf8')),
+  address: '0x99027c41f74b38862f53bda999881d8389fc6a92',
+  network: 'mainnet',
+}
+
+console.log(INFURA_API_KEY, WALLET_PRIVATE_KEY)
+
+////////////////////////
+////////////////////////
+////////////////////////
+
+const provider = new providers.InfuraProvider('mainnet', INFURA_API_KEY)
+
+const wallet = new Wallet(WALLET_PRIVATE_KEY, provider)
+console.log(wallet.address)
+
+const contract = new Contract(CONTRACT.address, CONTRACT.abi, wallet)
+
+//////////////////////////
+//////////////////////////
+//////////////////////////
+
+async function getIsBarOpen() {
+  return await contract.barIsOpen()
+}
+
+async function getIsBarOnlyFins() {
+  return await contract.onlyFins()
+}
+
+async function getBalanceOfDrink(accountAddress, drinkId) {
+  return await contract.balanceOf(accountAddress, drinkId)
+}
+
+async function getCurrentRoundDrinkId() {
+  const roundDuration = 15 // blocks
+  const currentRound = await contract.currentRound()
+  return BigNumber.from(roundDuration).sub(currentRound.div(5)) // same formula as _drinkToMint on contract
+}
+
+async function mintDrink() {
+  const tx = await contract.mint('0x0000000000000000000000000000000000000000', {
+    value: utils.parseEther('0.0001'),
+  })
+
+  return tx
+}
+
+//////////////////////////
+//////////////////////////
+//////////////////////////
+
+// Every 10 seconds
+// Check if bar open.
+// Check which round we are on.
+// Check if we've minted that drink yet.
+// if not, mint it.
+
+let timer = setInterval(async () => {
+  const isBarOpen = await getIsBarOpen()
+  const isBarOnlyFins = await getIsBarOnlyFins()
+
+  if (!isBarOpen || !isBarOnlyFins) {
+    console.log('Bar is closed!')
+    clearInterval(timer)
+    return
+  }
+
+  const currentDrinkId = await getCurrentRoundDrinkId()
+  const hasMintedDrink = (
+    await getBalanceOfDrink(wallet.address, currentDrinkId.toNumber())
+  ).gt(0)
+
+  if (hasMintedDrink) {
+    console.log(`Already minted drink ${currentDrinkId.toString()}`)
+    return
+  }
+
+  console.log(`Minting drink ${currentDrinkId.toNumber()}...`)
+  const tx = await mintDrink()
+  console.log(`Transaction Sent : ${tx.hash}`)
+  // wait for transaction to be confrimed
+  tx.wait().then((receipt) => {
+    console.log(
+      `Transaction ${tx.hash} confirmed in block ${receipt.blockNumber}!`,
+    )
+  })
+}, 10000)
